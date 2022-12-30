494cfa7dd47f0a34eee45da474eea9646068fa05
salmantln <50806616+salmantln@users.noreply.github.com>
2022-12-30 10:57:27 +0100

    User redirected to /blocked if user is blocked

--- server/middleware/checkBlockedUsers.js
+++ server/middleware/checkBlockedUsers.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
+// const express = require('express')
+// const router = express.Router()
+// const jwt = require('jsonwebtoken')
 
 const mongoose = require('mongoose'),
   Tutors = mongoose.model('tutor'),
