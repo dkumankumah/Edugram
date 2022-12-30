30511aec74afcffd8c1c64c30b56af3d4f305958
Daniel Kumankumah <danielkumankumah@gmail.com>
2022-12-30 11:49:18 +0100

    course page changes

--- src/pages/search/[subject].tsx
+++ src/pages/search/[subject].tsx
@@ -55,9 +55,6 @@ export default function Overview ({ tutors, subject }: PageProps) {
     const group = getRootProps()
     return (
         <Box>
-            <Flex>
-
-            </Flex>
             <Box
                 justifyContent = "center"
                 px={20}
